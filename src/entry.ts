4b0d9d40a5c0d008275e1603b5e9bf10049572fa
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2023-08-17 22:50:28 +0800

    Pyonify entry.ts

--- src/entry.ts
+++ src/entry.ts
@@ -6,17 +6,32 @@ console.log("Hello from Vendetta!");
 // Polyfill Promise.allSettled
 Promise.allSettled = (values: any) => Promise.all(values).catch();
 
-(async () => {
-    await(await import("./lib/caching")).default();
+Object.freeze = Object;
+Object.seal = Object;
 
-    import(".").then((m) => m.default()).catch((e) => {
+const oldDefineProperty = Object.defineProperty;
+// @ts-ignore
+Object.defineProperty = function defineProperty(...args) {
+    if (args[2].configurable == null && args[2].writable !== false) {
+        args[2].configurable = true
+    }
+    
+    return oldDefineProperty.apply(this, args)
+};
+
+(async () => {
+    const initNow = performance.now();
+    await (await import("./lib/caching")).default();
+
+    await import(".").then((m) => m.default()).catch((e) => {
         console.log(e?.stack ?? e.toString());
         alert([
-            "Failed to load Vendetta!\n",
+            "Failed to load (pyon-ified) Vendetta!\n",
             `Build Number: ${ClientInfoManager.Build}`,
             `Vendetta: ${__vendettaVersion}`,
             e?.stack || e.toString(),
         ].join("\n"));
     });
-    
+
+    console.log(`Pyondetta inited in ${performance.now() - initNow}ms`);
 })();
